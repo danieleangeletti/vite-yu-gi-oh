dfa8f7018536f3ac7dd48b3546625442428e8ee3
danieleangeletti <angelettidaniele@gmail.com>
2024-01-22 13:36:51 +0000

    importazione Axios e test API

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import AppFooter from "./components/AppFooter.vue";
+import axios from "axios";
 
 export default {
   data() {
@@ -13,6 +14,13 @@ export default {
     AppFooter,
   },
   methods: {},
+  mounted() {
+    axios
+      .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0")
+      .then((response) => {
+        console.log(response);
+      });
+  },
 };
 </script>
 
