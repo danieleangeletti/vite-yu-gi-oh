45bd6f6e4b83cf586f3dfb75da2c1ff453a814b8
danieleangeletti <146859541+danieleangeletti@users.noreply.github.com>
2024-01-22 13:07:53 +0000

    Initial commit

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <footer>FOOTER</footer>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<template>
+  <main>MAIN</main>
+</template>
+
+<style lang="scss" scoped></style>
